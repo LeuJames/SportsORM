1d8abaaa2a915bddbc6b970f1fb513bc4ce228ec
James Leu <leujase@gmail.com>
2020-06-12 15:26:08 -0500

    Initial Upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsORM.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsORM.Controllers
 {
@@ -29,7 +30,22 @@ namespace SportsORM.Controllers
         [HttpGet("level_1")]
         public IActionResult Level1()
         {
-            return View();
+          ViewBag.One = context.Leagues.Where(l => l.Name.Contains("Women")).ToList();
+          ViewBag.Two = context.Leagues.Where(l => l.Name.Contains("Hockey")).ToList();
+          ViewBag.Three = context.Leagues.Where(l => !l.Name.Contains("Football")).ToList();
+          ViewBag.Four = context.Leagues.Where(l => l.Name.Contains("Conference")).ToList();
+          ViewBag.Five = context.Leagues.Where(l => l.Name.Contains("Altantic")).ToList();   
+          ViewBag.Six = context.Teams.Where(t => t.Location == "Dallas").ToList();
+          ViewBag.Seven = context.Teams.Where(t => t.TeamName == "Raptors").ToList();
+          ViewBag.Eight = context.Teams.Where(t => t.Location.Contains("City")).ToList();      
+          ViewBag.Nine = context.Teams.Where(t => t.TeamName[0] == 'T').ToList();
+          ViewBag.Ten = context.Teams.OrderBy(t => t.Location).ToList();
+          ViewBag.Eleven = context.Teams.OrderByDescending(t => t.TeamName).ToList();
+          ViewBag.Twelve  = context.Players.Where(p => p.LastName == "Cooper").ToList(); 
+          ViewBag.Thirteen  = context.Players.Where(p => p.FirstName == "Joshua").ToList(); 
+          ViewBag.Fourteen  = context.Players.Where(p => p.LastName == "Cooper" && p.FirstName != "Joshua").ToList();
+          ViewBag.fifteen  = context.Players.Where(p => p.FirstName == "Alexander" || p.FirstName == "Wyatt").ToList();
+          return View();
         }
 
         [HttpGet("level_2")]
